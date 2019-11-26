b8d101241c2f14cd12ccc564bb5c563b120120db
Jitendra Rajpoot <devansh.forlife@gmail.com>
2019-11-26 14:38:51 +0530

    Bugs Fixed

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
 });
 app.engine(
   "handlebars",
